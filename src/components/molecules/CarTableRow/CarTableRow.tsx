6beb80f5d289bfc95aa689d478bd5879347c1373
Adam Cieslak <adamcieslak00@gmail.com>
2023-08-11 15:47:17 +0200

    Car table refactor

--- src/components/molecules/CarTableRow/CarTableRow.tsx
+++ src/components/molecules/CarTableRow/CarTableRow.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyledButton } from 'components/atoms/Button/Button';
 import { StyledTr, StyledTd } from './CarTableRow.styles'
-import { CarProps } from 'hooks/useCars';
+import { CarProps, CarStatusEnum, TransmissionEnum } from 'hooks/useCars';
 import { useNavigate } from 'react-router-dom';
 
 interface CarTableRowProps {
@@ -13,20 +13,36 @@ interface CarTableRowProps {
 const CarTableRow: React.FC<CarTableRowProps> = ({ index, handleDeleteCar, carData: { id, mark, model, transmission, enginePower, drivingLicenseCategory, numberOfSeats, pricePerDay, registrationNumber, vin, status } }) => {
     const navigate = useNavigate();
 
+    const getTransmissionComponent = () => {
+        switch (transmission) {
+            case TransmissionEnum.Authomatic: return <StyledTd>Automatyczna</StyledTd>;
+            case TransmissionEnum.Manual: return <StyledTd>Manualna</StyledTd>;
+        };
+    };
+
+    const getCarStatusComponent = () => {
+        switch (status) {
+            case CarStatusEnum.Avaliable: return <StyledTd $status='succes'>Dostępny</StyledTd>;
+            case CarStatusEnum.InService: return <StyledTd $status='inProgress'>Serwisowany</StyledTd>;
+            case CarStatusEnum.OutOfOrder: return <StyledTd $status='warning'>Niesprawny</StyledTd>;
+            case CarStatusEnum.Rented: return <StyledTd $status='inProgress'>Wynajęty</StyledTd>;
+        };
+    };
+
     return (
         <StyledTr>
             <StyledTd>{`${index + 1}.`}</StyledTd>
             <StyledTd>{id}</StyledTd>
             <StyledTd>{mark}</StyledTd>
             <StyledTd>{model}</StyledTd>
-            <StyledTd>{transmission}</StyledTd>
+            <StyledTd>{getTransmissionComponent()}</StyledTd>
             <StyledTd>{`${enginePower} KM`}</StyledTd>
             <StyledTd>{drivingLicenseCategory}</StyledTd>
             <StyledTd>{numberOfSeats}</StyledTd>
             <StyledTd>{`${pricePerDay.toFixed(2)} zł.`}</StyledTd>
             <StyledTd>{registrationNumber}</StyledTd>
             <StyledTd>{vin}</StyledTd>
-            <StyledTd>{status}</StyledTd>
+            <StyledTd>{getCarStatusComponent()}</StyledTd>
             <StyledTd><StyledButton onClick={() => handleDeleteCar(id)}>Usuń</StyledButton></StyledTd>
             <StyledTd><StyledButton onClick={() => navigate(`/editCar/${id}`)}>Edytuj</StyledButton></StyledTd>
         </StyledTr>
