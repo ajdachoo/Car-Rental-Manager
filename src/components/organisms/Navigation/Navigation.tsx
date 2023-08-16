780630154984a1ff7f2d4827c572d3f6373e2c3c
Adam Cieslak <adamcieslak00@gmail.com>
2023-08-16 16:33:17 +0200

    Refactor navigation

--- src/components/organisms/Navigation/Navigation.tsx
+++ src/components/organisms/Navigation/Navigation.tsx
@@ -3,6 +3,12 @@ import { Wrapper, Logo, HomeLink, NavSection, StyledNavLink } from './Navigation
 import { ReactComponent as HomeIcon } from 'assets/icons/house-solid.svg';
 import { ReactComponent as NewIcon } from 'assets/icons/pen-to-square-solid.svg';
 import { ReactComponent as TableListIcon } from 'assets/icons/table-list-solid.svg';
+import { ReactComponent as CheckIcon } from 'assets/icons/check-solid.svg';
+import { ReactComponent as WrenchIcon } from 'assets/icons/screwdriver-wrench-solid.svg';
+import { ReactComponent as XMarkIcon } from 'assets/icons/xmark-solid.svg';
+import { ReactComponent as ClockIcon } from 'assets/icons/clock-regular.svg';
+import { ReactComponent as ExclamationTriangleIcon } from 'assets/icons/triangle-exclamation-solid.svg';
+import { ReactComponent as SpinnerIcon } from 'assets/icons/spinner-solid.svg';
 
 const Navigation: React.FC = () => {
     return (
@@ -15,18 +21,26 @@ const Navigation: React.FC = () => {
             </HomeLink>
             <NavSection>
                 <p>Pojazdy</p>
-                <StyledNavLink to="/cars"><TableListIcon />Tabela pojazdów</StyledNavLink>
-                <StyledNavLink to="/addCar"><NewIcon />Dodaj nowy pojazd</StyledNavLink>
+                <StyledNavLink to="/cars"><TableListIcon />Wszystkie</StyledNavLink>
+                <StyledNavLink to="/addCar"><NewIcon />Dodaj nowy</StyledNavLink>
+                <StyledNavLink to="/cars"><CheckIcon />Dostępne</StyledNavLink>
+                <StyledNavLink to="/cars"><SpinnerIcon />Wypożyczone</StyledNavLink>
+                <StyledNavLink to="/cars"><ExclamationTriangleIcon />Niesprawne</StyledNavLink>
+                <StyledNavLink to="/cars"><WrenchIcon />Serwisowane</StyledNavLink>
             </NavSection>
             <NavSection>
                 <p>Klienci</p>
-                <StyledNavLink to="/clients"><TableListIcon />Tabela klientów</StyledNavLink>
-                <StyledNavLink to="/addClient"><NewIcon />Dodaj nowego klienta</StyledNavLink>
+                <StyledNavLink to="/clients"><TableListIcon />Wszyscy</StyledNavLink>
+                <StyledNavLink to="/addClient"><NewIcon />Dodaj nowego</StyledNavLink>
+                <StyledNavLink to="/clients"><XMarkIcon />Zablokowani</StyledNavLink>
             </NavSection>
             <NavSection>
                 <p>Wypożyczenia</p>
                 <StyledNavLink to="/rentals"><TableListIcon />Tabela wypożyczeń</StyledNavLink>
-                <StyledNavLink to="/addRental"><NewIcon />Stwórz wypożyczenie</StyledNavLink>
+                <StyledNavLink to="/addRental"><NewIcon />Stwórz nowe</StyledNavLink>
+                <StyledNavLink to="/rentals"><SpinnerIcon />Aktywne</StyledNavLink>
+                <StyledNavLink to="/rentals"><ClockIcon />Opóźnione</StyledNavLink>
+                <StyledNavLink to="/rentals"><CheckIcon />Zakończone</StyledNavLink>
             </NavSection>
         </Wrapper>
     );
